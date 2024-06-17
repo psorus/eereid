b45677ad8fe18e4df27a243ce3c3da4051283c26
Simon Klüttermann <Simon.kluettermann@gmx.de>
2024-06-17 11:04:07 +0200

    added comments to the first example

--- examples/simple_model.py
+++ examples/simple_model.py
@@ -1,14 +1,14 @@
 import eereid as ee
 
-g=ee.ghost(model=ee.models.conv(),
-           dataset=ee.datasets.mnist(),
-           loss=ee.losses.extended_triplet(),
-           novelty=ee.novelty.distance(),
-           preproc=ee.prepros.subsample(0.1),
-           triplet_count=100,
-           crossval=True)
+g=ee.ghost(model=ee.models.conv(),              #Use a simple convolutional model
+           dataset=ee.datasets.mnist(),         #Train on mnist dataset
+           loss=ee.losses.extended_triplet(),   #Use Extended Triplet loss (+d(p,n)) for training
+           novelty=ee.novelty.distance(),       #Evaluate also novelty detection. Far away=novel
+           preproc=ee.prepros.subsample(0.1),   #To speed up training, use only 10% of samples
+           triplet_count=100,                   #To speed up training, use only 100 triplets for training
+           crossval=True)                       #Enable crossvalidation
 
-acc=g.evaluate()
+acc=g.evaluate()                                #Evaluate the model
 
 print(acc)
 
