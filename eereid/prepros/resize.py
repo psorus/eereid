33178fccfd62dc90e8637e5e82ddad2b9e752a71
Simon Klüttermann <Simon.kluettermann@gmx.de>
2024-08-12 11:08:37 +0200

    small bugfix

--- eereid/prepros/resize.py
+++ eereid/prepros/resize.py
@@ -17,6 +17,7 @@ class resize(prepro):
         return cv2.resize(image, self.size,interpolation=interpolation)
 
     def _apply_special(self,eereid):
+        eereid.input_shape=list(eereid.input_shape)
         eereid.input_shape[0]=self.size[0]
         eereid.input_shape[1]=self.size[1]
 
