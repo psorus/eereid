0dee0895d7fd395790cc2dea62529ee0c888871f
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2024-09-26 13:10:08 +0200

    created custom function for error message

--- eereid/importhelper.py
+++ eereid/importhelper.py
@@ -7,7 +7,10 @@ class importhelper():
         self.install = install
 
     def __getattr__(self, name):
-        raise ImportError(f"""Using the '{self.job}' module requires the '{self.lib}' library. Please install it by running: '{self.install}'""")
-
+        self.raiseError()
+        
     def __call__(self, *args, **kwds):
+        self.raiseError()
+        
+    def raiseError(self):
         raise ImportError(f"""Using the '{self.job}' module requires the '{self.lib}' library. Please install it by running: '{self.install}'""")
