7ee0b37967f999b5fcca2d577015c1a2f2058194
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2024-09-26 14:54:43 +0200

    moved preprocessing to ghost init

--- tests/test_ghost.py
+++ tests/test_ghost.py
@@ -5,10 +5,10 @@ class GhostTest(unittest.TestCase):
     def test_evaluate(self):
         g=ee.ghost(model=ee.models.conv(),
            dataset=ee.datasets.mnist(),
+           preprocessing=ee.prepros.subsample(0.1),
            prepros=ee.prepros.apply_func())
         
         #just to speed up the evaluation
-        g(ee.prepros.subsample(0.1))
         g["triplet_count"]=1000
         g["epochs"] = 1
         
